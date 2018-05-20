7050cbea49b2f9f51ab3f2742e8f5e957db709d1
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2018-05-20 15:48:34 +0200

    Made formatDateTime part of starter code for lesson

--- lessons/lesson-3/api.js
+++ lessons/lesson-3/api.js
@@ -1,5 +1,15 @@
 import moment from 'moment';
 
+export function formatDateTime(dateString) {
+  const parsed = moment(new Date(dateString));
+
+  if (!parsed.isValid()) {
+    return dateString;
+  }
+
+  return parsed.format('H A on D MMM YYYY');
+}
+
 export function formatDate(dateString) {
   const parsed = moment(new Date(dateString));
 
